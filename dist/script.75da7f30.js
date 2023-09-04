e385e5af791d17c6575a4ea1d30872fa974a160d
Mapelujo <olamideiyanda18@gmail.com>
2023-09-04 11:57:58 +0100

    progress

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -309,7 +309,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50287" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35117" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
